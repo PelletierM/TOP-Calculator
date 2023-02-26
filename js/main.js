88a2470f1a6d878df7cdcd6e94a7cfd7cdf33f67
Maxime Pelletier <122843454+PelletierM@users.noreply.github.com>
2023-02-25 22:31:57 -0500

    Create conditions for valid input
    Fix CSS class syntax
    Add functions that prevent invalid input
    Add history functionality
    Next: Fix bug where only one point can ever be added
    Next: Add number size limit and round total
    Next: Add history panel

--- js/main.js
+++ js/main.js
@@ -1,77 +1,21 @@
-let userInput = [];
-
 // CALCULATE FUNCTIONS
 function calculateSubTotal(a, b, operator) {
     if (operator === "+") {return +a + +b}
     else if (operator === "-") {return +a - +b}
-    else if (operator === "*") {return +a * +b}
+    else if (operator === "x") {return +a * +b}
     else if (operator === "/") {return +a / +b} // Find a way to return error when dividing by 0
     else if (operator === "^") {return Math.pow(+a, +b)}
 }
-/*  
-    I have made 2 different functions to calculate the result of a given array.
-    The first one loops over the whole array for every pair of parenthesis,
-    then loops over the whole array 5 more times, one for each operator and in order of priority
-    The second one uses a recursive function to parse the array. It's much more complex to understand
-    than the first one, but has the advantage of going over the whole array only once.
-    Both solutions have a time complexity of O(n), but after some testing the second one seems
-    to be twice as fast as the first one. It also allows me to add more operators in the future,
-    without having to change the function's code.
-*/
 
-// THIS IS THE FIRST CALCULATE OPTION - Loop complete array once for each operator
-function calculateOperator(input, operator) {
-    let indexStartOp = 0;
-    let indexEndOp = 0;
-    while (input.some(item => item === operator)) {
-        for (let i = 0; i < input.length; i++) {
-            if(input[i] === operator) {indexStartOp = (i-1); indexEndOp = (i+1); break}
-        }
-        let subTotalOp = calculateSubTotal(+input[indexStartOp], +input[indexEndOp], operator);
-        let tempArrayOp1 = input.slice(0, indexStartOp);
-        let tempArrayOp2 = subTotalOp
-        let tempArrayOp3 = input.slice((indexEndOp + 1))
-        input = tempArrayOp1.concat(tempArrayOp2).concat(tempArrayOp3)
-    }
-    return input
-}
-
-function combineParenthesis(input) {
-    while (input.some(item => item === "(")) {   
-        let indexStartParenthesis = 0;
-        let indexEndParenthesis = 0;
-        for (let i = 0; i < input.length; i++) {
-            if(input[i] === "("){indexStartParenthesis = i}
-            else if (input[i] === ")") {indexEndParenthesis = i; break};
-        }
-        if (indexStartParenthesis > 0 && !isNaN(+input[(indexStartParenthesis - 1)])){
-            input.splice(indexStartParenthesis, 0, "*");
-            indexStartParenthesis += 1;
-            indexEndParenthesis += 1;
-        }
-        let subTotalParenthesis = input.slice((indexStartParenthesis + 1), indexEndParenthesis);
-        subTotalParenthesis = calculate(subTotalParenthesis)
-        let tempArrayParenthesis = []
-        input = tempArrayParenthesis.concat(input.slice(0, indexStartParenthesis), subTotalParenthesis, input.slice((indexEndParenthesis+1)));
-    }
-    return input
-}
-
-function calculate(input) {
-    input = calculateOperator(calculateOperator(calculateOperator(calculateOperator(calculateOperator((combineParenthesis(input)), "^"), "/"), "*"), "-"), "+");
-    return input
-    }
-
-// THIS IS THE SECOND CALCULATE OPTION - Parser with recursive function
 let operatorPrecedence = {
     "^": 3, 
     "/": 2,
-    "*": 2,
+    "x": 2,
     "-": 1,
     "+": 1,
 }
 
-function calculationTest(input, insideParenthesis = false, previousStackPrecedence = 5) {
+function calculate(input, insideParenthesis = false, previousStackPrecedence = 5) {
     let parsedInput = [];
     let currentPrecedence = 0;
     let unparsedInput = [];
@@ -92,7 +36,7 @@ function calculationTest(input, insideParenthesis = false, previousStackPreceden
                 }
                 else {
                     tempArray = input.slice(i);
-                    input = parsedInput.concat(calculationTest(tempArray, false, currentPrecedence));
+                    input = parsedInput.concat(calculate(tempArray, false, currentPrecedence));
                     --i;
                 }
             }
@@ -115,7 +59,7 @@ function calculationTest(input, insideParenthesis = false, previousStackPreceden
         else if (input[i] === "(") {
             input.splice(i, 1);
             tempArray = input.slice(i);
-            input = parsedInput.concat(calculationTest(tempArray, true, currentPrecedence));
+            input = parsedInput.concat(calculate(tempArray, true, currentPrecedence));
             --i;
         }
         // Fin a way to return error here
@@ -126,21 +70,112 @@ function calculationTest(input, insideParenthesis = false, previousStackPreceden
     return parsedInput.concat(unparsedInput)
 }
 
-// BUTTON EVENT LISTENERS
-const currentOperationText = document.querySelector(".currentOperationText")
-const resultText = document.querySelector(".resultText")
+let activeArray = [];
+let activeString = "";
+let numCounter = 0;
+let operatorMode = false;
+let parenthesisCount = 0;
+let ans = "";
+let history = {}
 
-const allButtons = document.querySelector(".allButtons")
+// BUTTON EVENT LISTENERS
+const calculationText = document.querySelector(".calculation-text")
+const activeText = document.querySelector(".active-text")
+
+const allButtons = document.querySelector(".all-buttons")
 allButtons.addEventListener("click", function(e){
     if(e.target.nodeName === "BUTTON") {
-    userInput.push(e.target.innerText)
+        if(e.target.classList.contains("number")) appendNumber(e);
+        else if(e.target.classList.contains("point")) appendPoint(e);
+        else if(e.target.classList.contains("operator")) appendOperator(e);
+        else if(e.target.classList.contains("open-parenthesis")) appendOpenParenthesis(e);
+        else if(e.target.classList.contains("closed-parenthesis")) appendClosedParenthesis(e);
+        activeText.innerText = activeString;
+        if(e.target.classList.contains("equal")) triggerEqual();
     }
-    currentOperationText.innerText = userInput.join("")
-    equalButton(e);
 })
 
-function equalButton(e) {
-    if(e.target.innerText === "=") {
-    resultText.innerText = calculate(userInput).join("");
+function appendNumber(e) {
+    if (checkClosedParenthesis()) {
+        activeArray.push("x") ;
+        activeString += "x" ;        
     }
+    operatorMode = false;
+    activeString += e.target.innerText;
+    ++numCounter;
+}
+
+function appendPoint(e) {
+    if (!activeString.includes(".")) {
+        activeString += e.target.innerText;
+        ++numCounter;
+    }
+}
+
+function appendOperator(e) {
+    if (!checkOpenParenthesis() && activeString.length > 0) {    
+        if (operatorMode) {
+            activeArray.splice(-1, 1, e.target.innerText)
+            activeString = activeString.slice(0, -1) + e.target.innerText;
+        }
+        else {
+            pushString();
+            activeArray.push(e.target.innerText);
+            activeString += e.target.innerText;
+            operatorMode = true;
+        }
+    }
+    
+}
+
+    function pushString() {
+        if (numCounter > 0) {
+            activeArray.push(activeString.slice(-numCounter));
+        }
+        numCounter = 0;
+    }
+
+function appendOpenParenthesis(e) {
+    if (numCounter > 0 || checkClosedParenthesis()) {
+        pushString();
+        activeArray.push("x", "(") ;
+        activeString += "x(" ;
+    }
+    else {
+        activeArray.push("(");
+        activeString += "(";
+    }
+    ++parenthesisCount;
+}
+
+function appendClosedParenthesis(e){
+    if (!operatorMode && parenthesisCount > 0 && !checkOpenParenthesis()) {
+        pushString();
+        activeArray.push(")");
+        activeString += ")";
+        --parenthesisCount;
+    }
+}
+
+function checkOpenParenthesis() {
+   return activeString.slice(-1) === "(";
+}
+
+function checkClosedParenthesis() {
+   return activeString.slice(-1) === ")";
+}
+
+function triggerEqual() {
+    pushString();
+    while (parenthesisCount > 0) {
+        activeArray.push(")");
+        activeString += ")";
+        --parenthesisCount;       
+    }
+    ans = calculate(activeArray).join("");
+    history[`${Object.keys(history).length+1}`] = [activeString, ans]
+    calculationText.innerText = activeString;
+    activeText.innerText = ans;
+    activeString = "";
+    activeArray = [];
 }
