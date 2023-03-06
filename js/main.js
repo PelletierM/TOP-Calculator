43e259be5688c091db408a876ec05b1f8344c504
Maxime Pelletier <122843454+PelletierM@users.noreply.github.com>
2023-03-05 19:48:55 -0500

    Improve styling and clean up code
    Improve CSS styling
    Improve history text layout
    Fix keyboard listeners
    Clean up HTML, CSS and JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/* Always make sure these constants match with the html button value */
+/* Always make sure these const match with the html button value */
 const exponent = "^";
 const multiplication = " x ";
 const division = " / ";
@@ -11,6 +11,287 @@ const plusMinus = "-"
 const equal = " = "
 const backspace = "backspace"
 const errorMessage = "ERROR"
+
+/* 
+INPUT FUNCTIONS 
+*/
+let activeArray = [];
+let activeString = "";
+let numCounter = 0;
+const maxNumCounter = 15;
+let lastOperatorLength = 0;
+let parenthesisCount = 0;
+let ans = {
+    value: undefined
+}
+let history = {}
+
+    /* 
+    CHECK FUNCTIONS 
+    */
+function checkPlusMinusState() {
+    return activeString.charAt(activeString.length - numCounter - 1) === plusMinus;
+}
+
+function checkOpenParenthesis() {
+   return activeString.slice(-1) === openParenthesis;
+}
+
+function checkClosedParenthesis() {
+   return activeString.slice(-1) === closedParenthesis;
+}
+
+function checkAnsLast() {
+    return (typeof activeArray[activeArray.length - 1] === "object");
+}
+
+    /* 
+    APPEND FUNCTIONS 
+    */
+function pushString() {
+    if (numCounter !== 0 && !checkAnsLast()) {
+        if (checkPlusMinusState()) {
+            numCounter += 1;
+        }
+        activeArray.push(activeString.slice(-numCounter));
+    }
+    numCounter = 0;
+}
+
+function appendNumber(value) {
+    if (checkClosedParenthesis() || checkAnsLast()) {
+        appendOperator(multiplication);        
+    }
+    if (numCounter === 1 && activeString.slice(-1) == "0") {
+        activeString = activeString.slice(0, -1);
+        --numCounter;
+    }
+    if (numCounter < maxNumCounter){
+        lastOperatorLength = 0;
+        activeString += value;
+        ++numCounter;
+    }
+}
+
+function appendPoint(value) {
+    if (numCounter === 0) {
+        appendNumber("0")
+    }
+    if (!activeString.slice(-numCounter).includes(point) && numCounter < maxNumCounter) {
+        lastOperatorLength = 0;
+        activeString += value;
+        ++numCounter;
+    }
+}
+
+function togglePlusMinus(value) {
+    if (numCounter === 0 && !checkPlusMinusState()) {
+        appendNumber("0");
+    }
+    if (!checkPlusMinusState() && !checkAnsLast()) {
+        activeString = activeString.slice(0, -numCounter) + value + activeString.slice(-numCounter);
+    }
+    else if(checkPlusMinusState()) {
+        activeString = activeString.slice(0, activeString.length - numCounter - 1) + activeString.slice(activeString.length - numCounter);
+    }
+}
+
+function appendOperator(value) {
+    if (activeString.length === 0 && ans.value !== undefined) {
+        activeArray.push(ans, value);
+        activeString += "Ans" + value;
+        lastOperatorLength = value.length;
+    }
+    if (!checkOpenParenthesis() && activeString.length !== 0 && (activeString.charAt(activeString.length - 1) !== plusMinus)) {    
+        if (lastOperatorLength > 0) {
+            activeArray.splice(-1, 1, value);
+            activeString = activeString.slice(0, -lastOperatorLength) + value;
+            lastOperatorLength = value.length;
+        }
+        else {
+            pushString();
+            activeArray.push(value);
+            activeString += value;
+            lastOperatorLength = value.length;
+        }
+    }  
+}
+
+function appendOpenParenthesis() {
+    if (numCounter > 0 || checkClosedParenthesis() || checkAnsLast()) {
+        pushString();
+        activeArray.push(multiplication, openParenthesis) ;
+        activeString += multiplication + openParenthesis ;
+    }
+    else {
+        activeArray.push(openParenthesis);
+        activeString += openParenthesis;
+    }
+    ++parenthesisCount;
+}
+
+function appendClosedParenthesis(){
+    if (lastOperatorLength === 0 && parenthesisCount > 0 && !checkOpenParenthesis()) {
+        pushString();
+        activeArray.push(closedParenthesis);
+        activeString += closedParenthesis;
+        --parenthesisCount;
+    }
+}
+
+function appendAns(){
+    if(ans.value !== undefined && !checkAnsLast()) {
+        if (checkClosedParenthesis() || numCounter > 0) {
+            appendOperator(multiplication);       
+        }
+        activeArray.push(ans);
+        activeString += "Ans";
+        lastOperatorLength = 0;
+        numCounter = 0;
+    }
+}
+
+    /* 
+    REMOVE FUNCTIONS 
+    */
+function executeBackspace() {
+    if(activeString.length > 0) {
+        pushString();
+        if (checkOpenParenthesis()) removeOpenParenthesis();
+        else if (checkClosedParenthesis()) removeClosedParenthesis();
+        else if (!isNaN(+activeArray[activeArray.length - 1])) removeNumber()
+        else if (Object.keys(operatorPrecedence).includes(activeArray[activeArray.length - 1])) removeOperator();
+        else if (checkAnsLast()) removeAns();
+    }
+}
+
+function removeLastNaN() {
+    activeString = activeString.slice(0, (activeString.length - activeArray[activeArray.length - 1].length))
+    activeArray.pop();
+}
+
+function popActiveArrayNum() {
+    numCounter = activeArray[activeArray.length - 1].length;
+    numCounter -= 1;
+    if (checkPlusMinusState()) {
+        numCounter -= 1;
+    }
+    numCounter +=1;
+    activeArray.pop();
+}
+
+function removeOpenParenthesis() {
+    removeLastNaN();
+    --parenthesisCount;
+    if (activeArray.length > 0) {
+    lastOperatorLength = activeArray[activeArray.length - 1].length;}  
+}
+
+function removeClosedParenthesis() {
+    removeLastNaN();
+    ++parenthesisCount;
+    lastOperatorLength = 0
+    popActiveArrayNum();   
+}
+
+function removeOperator() {
+    removeLastNaN()
+    lastOperatorLength = 0;
+    numCounter = 0;
+    if (!checkClosedParenthesis() && !checkAnsLast()) {
+        popActiveArrayNum(); 
+    }
+}
+
+function removeAns() {
+    activeString = activeString.slice(0, activeString.length - 3)
+    activeArray.pop();
+    if(activeArray.length > 0) {
+        lastOperatorLength = activeArray[activeArray.length - 1].length;
+    }
+    numCounter = 0;
+}
+
+function removeNumber() {
+    popActiveArrayNum();
+    if(checkPlusMinusState() && numCounter <= 1) {
+        togglePlusMinus();
+    }
+    activeString = activeString.slice(0, activeString.length - 1)
+    --numCounter
+    if(numCounter === 0 && activeArray.length > 0) {
+        lastOperatorLength = activeArray[activeArray.length - 1].length;
+    }
+}
+
+    /* 
+    EQUAL FUNCTIONS 
+    */
+function triggerEqual() {
+    if(activeString !== "" && lastOperatorLength === 0 && !checkOpenParenthesis()){
+        pushString();
+        while (parenthesisCount > 0) {
+            activeArray.push(closedParenthesis);
+            activeString += closedParenthesis;
+            --parenthesisCount;       
+        }
+        executeCalculate();
+    }
+    else if(activeString === "" && Object.keys(history).length > 0) {
+        activeArray = history[Object.keys(history).length][2];
+        activeString = history[Object.keys(history).length][0];
+        executeCalculate();
+    }
+}
+
+function executeCalculate() {
+    let tempArray = activeArray.slice(0);
+    ans.value = calculate(tempArray)
+    resultText.innerText = `${activeString} =`;
+    if (isNaN(ans.value)) {
+        activeText.innerText = errorMessage;
+        ans.value = undefined;
+    }
+    else {
+        activeText.innerText = Math.round(1000000 * ans.value)/1000000;
+    }
+    addToHistory()
+    resetVariables()
+}
+
+function addToHistory() {
+    /* CSS is column-reverse, order elements accordingly */
+    history[`${Object.keys(history).length+1}`] = [activeString, ans.value, activeArray]
+    let formulaPara = document.createElement("p");
+    let formulaParaText = document.createTextNode(`${activeString} =`);
+    formulaPara.appendChild(formulaParaText);
+    let ansPara = document.createElement("p");
+    let ansParaText = document.createTextNode(`${activeText.innerText}`);
+    ansPara.appendChild(ansParaText);
+    if (historyText.firstChild) {
+        formulaPara.classList.add("para-margin");
+    }
+    historyText.prepend(ansPara, formulaPara);
+}
+
+function resetVariables() {
+    activeArray = [];
+    activeString = "";
+    numCounter = 0;
+    lastOperatorLength = 0;
+    parenthesisCount = 0;
+}
+
+function clearAll() {
+    resetVariables()
+    ans.value = undefined;
+    activeText.innerText = "";
+    resultText.innerText = "";
+}
+
+/* 
+CALCULATE FUNCTIONS 
+*/
 let operatorPrecedence = {
     [exponent]: 3, 
     [division]: 2,
@@ -19,7 +300,6 @@ let operatorPrecedence = {
     [addition]: 1,
 }
 
-// CALCULATE FUNCTIONS
 function calculateSubTotal(a, b, operator) {
     if (operator == addition) {return +a + +b}
     else if (operator == substraction) {return +a - +b}
@@ -86,26 +366,17 @@ function calculate(input, insideParenthesis = false, previousStackPrecedence = 5
     return parsedInput.concat(unparsedInput)
 }
 
-let activeArray = [];
-let activeString = "";
-let numCounter = 0;
-const maxNumCounter = 15;
-let lastOperatorLength = 0;
-let parenthesisCount = 0;
-let ans = {
-    value: undefined
-}
-let history = {}
-
-// BUTTON EVENT LISTENERS
+/* 
+CLICK AND KEYBOARD EVENT LISTENERS 
+*/
 const historyText = document.querySelector(".history-text")
-const calculationText = document.querySelector(".calculation-text")
+const resultText = document.querySelector(".result-text")
 const activeText = document.querySelector(".active-text")
 const calculatorContainer = document.querySelector(".calculator")
 calculatorContainer.addEventListener("click", function(e){
     if(e.target.nodeName === "BUTTON") {
-        if(activeString.length === 0 && ans.value !== undefined) {calculationText.innerText = `Ans = ${ans.value}`}
-        else if(activeText.innerText == errorMessage) {calculationText.innerText = `Ans = ${errorMessage}`};
+        if(activeString.length === 0 && ans.value !== undefined) {resultText.innerText = `Ans = ${Math.round(100000000 * ans.value)/100000000}`}
+        else if(activeText.innerText == errorMessage) {resultText.innerText = `Ans = ${errorMessage}`};
         if(e.target.classList.contains("number")) appendNumber(e.target.value)
         else if(e.target.classList.contains("point")) appendPoint(e.target.value)
         else if(e.target.classList.contains("plus-minus")) togglePlusMinus(e.target.value)
@@ -123,268 +394,63 @@ calculatorContainer.addEventListener("click", function(e){
 })
 
 const allButtons = document.querySelectorAll("button")
+const keyboardInputs = {
+    "0": "0",
+    "1": "1",
+    "2": "2",
+    "3": "3",
+    "4": "4",
+    "5": "5",
+    "6": "6",
+    "7": "7",
+    "8": "8",
+    "9": "9",
+    ".": point,
+    ",": point,
+    "(": openParenthesis,
+    ")": closedParenthesis,
+    "*": multiplication,
+    "x": multiplication,
+    "/": division,
+    "+": addition,
+    "-": substraction,
+    "±": plusMinus,
+    "^": exponent,
+    "Enter": equal,
+    "Escape": "clear-all",
+    "Delete": "clear-all",
+    "Backspace": backspace,
+    "a": "ans",
+    "h": "toggle-history"
+}
+
+function checkKeyboardInput(e) {
+    if (e.key in keyboardInputs) {
+    return keyboardInputs[e.key]
+    }
+}
+
 document.addEventListener("keydown", function(e){
+    let currentKeyValue = checkKeyboardInput(e);
     for (let button of allButtons) {
-        let keyValue = e.key.slice(0);
-        if (keyValue === "*") {keyValue = multiplication}
-        if (keyValue === "Enter") {keyValue = equal}
-        if (keyValue === "Backspace") {keyValue = backspace}
-        if (keyValue === ",") {keyValue = point}
-        if (button.value.includes(keyValue)) {
+        if (button.value === currentKeyValue) {
             button.click();
+            button.classList.add("active")
+        }
+    }
+})
+document.addEventListener("keyup", function(e){
+    let currentKeyValue = checkKeyboardInput(e);
+    for (let button of allButtons) {
+        if (button.value === currentKeyValue) {
+            button.classList.remove("active")
         }
     }
 })
 
-// CHECK FUNCTIONS
-function checkPlusMinusState() {
-    return activeString.charAt(activeString.length - numCounter - 1) === plusMinus;
-}
-
-function checkOpenParenthesis() {
-   return activeString.slice(-1) === openParenthesis;
-}
-
-function checkClosedParenthesis() {
-   return activeString.slice(-1) === closedParenthesis;
-}
-
-function checkAnsLast() {
-    return (typeof activeArray[activeArray.length - 1] === "object");
-}
-
-// APPEND FUNCTIONS
-function pushString() {
-    if (numCounter !== 0 && !checkAnsLast()) {
-        if (checkPlusMinusState()) {
-            numCounter += 1;
-        }
-        activeArray.push(activeString.slice(-numCounter));
-    }
-    numCounter = 0;
-}
-
-function appendNumber(value) {
-    if (checkClosedParenthesis() || checkAnsLast()) {
-        appendOperator(multiplication);        
-    }
-    if (numCounter === 1 && activeString.slice(-1) == "0") {
-        activeString = activeString.slice(0, -1);
-        --numCounter;
-    }
-    if (numCounter < maxNumCounter){
-        lastOperatorLength = 0;
-        activeString += value;
-        ++numCounter;
-    }
-}
-
-function appendPoint(value) {
-    if (numCounter === 0) {
-        appendNumber("0")
-    }
-    if (!activeString.slice(-numCounter).includes(point) && numCounter < maxNumCounter) {
-        lastOperatorLength = 0;
-        activeString += value;
-        ++numCounter;
-    }
-}
-
-// YOU ARE HERE - FIX INPUT KEYBOARD + BACKSPACE
-function togglePlusMinus(value) {
-    if (numCounter === 0 && !checkPlusMinusState()) {
-        appendNumber("0");
-    }
-    if (!checkPlusMinusState() && !checkAnsLast()) {
-        activeString = activeString.slice(0, -numCounter) + value + activeString.slice(-numCounter);
-    }
-    else if(checkPlusMinusState()) {
-        activeString = activeString.slice(0, activeString.length - numCounter - 1) + activeString.slice(activeString.length - numCounter);
-    }
-}
-
-function appendOperator(value) {
-    if (activeString.length === 0 && ans.value !== undefined) {
-        activeArray.push(ans, value);
-        activeString += "Ans" + value;
-        lastOperatorLength = value.length;
-    }
-    if (!checkOpenParenthesis() && activeString.length !== 0 && (activeString.charAt(activeString.length - 1) !== plusMinus)) {    
-        if (lastOperatorLength > 0) {
-            activeArray.splice(-1, 1, value);
-            activeString = activeString.slice(0, -lastOperatorLength) + value;
-            lastOperatorLength = value.length;
-        }
-        else {
-            pushString();
-            activeArray.push(value);
-            activeString += value;
-            lastOperatorLength = value.length;
-        }
-    }  
-}
-
-function appendOpenParenthesis() {
-    if (numCounter > 0 || checkClosedParenthesis() || checkAnsLast()) {
-        pushString();
-        activeArray.push(multiplication, openParenthesis) ;
-        activeString += multiplication + openParenthesis ;
-    }
-    else {
-        activeArray.push(openParenthesis);
-        activeString += openParenthesis;
-    }
-    ++parenthesisCount;
-}
-
-function appendClosedParenthesis(){
-    if (lastOperatorLength === 0 && parenthesisCount > 0 && !checkOpenParenthesis()) {
-        pushString();
-        activeArray.push(closedParenthesis);
-        activeString += closedParenthesis;
-        --parenthesisCount;
-    }
-}
-
-function appendAns(){
-    if(ans.value !== undefined) {
-        if (checkClosedParenthesis() || numCounter > 0) {
-            appendOperator(multiplication);       
-        }
-        activeArray.push(ans);
-        activeString += "Ans";
-        lastOperatorLength = 0;
-        numCounter = 0;
-    }
-}
-
-// REMOVE FUNCTIONS
-function executeBackspace() {
-    if(activeString.length > 0) {
-        pushString();
-        if (checkOpenParenthesis()) removeOpenParenthesis();
-        else if (checkClosedParenthesis()) removeClosedParenthesis();
-        else if (!isNaN(+activeArray[activeArray.length - 1])) removeNumber()
-        else if (Object.keys(operatorPrecedence).includes(activeArray[activeArray.length - 1])) removeOperator();
-        else if (checkAnsLast()) removeAns();
-    }
-}
-
-function removeLastNaN() {
-    activeString = activeString.slice(0, (activeString.length - activeArray[activeArray.length - 1].length))
-    activeArray.pop();
-}
-
-function removeOpenParenthesis() {
-    removeLastNaN();
-    --parenthesisCount;
-    lastOperatorLength = activeArray[activeArray.length - 1].length;   
-}
-
-function popActiveArrayNum() {
-    numCounter = activeArray[activeArray.length - 1].length;
-    numCounter -= 1;
-    if (checkPlusMinusState()) {
-        numCounter -= 1;
-    }
-    numCounter +=1;
-    activeArray.pop();
-}
-
-function removeClosedParenthesis() {
-    removeLastNaN();
-    ++parenthesisCount;
-    lastOperatorLength = 0
-    popActiveArrayNum();   
-}
-
-function removeOperator() {
-    removeLastNaN()
-    lastOperatorLength = 0;
-    numCounter = 0;
-    if (!checkClosedParenthesis() && !checkAnsLast()) {
-        popActiveArrayNum(); 
-    }
-}
-
-function removeAns() {
-    activeString = activeString.slice(0, activeString.length - 3)
-    activeArray.pop();
-    if(activeArray.length > 0) {
-        lastOperatorLength = activeArray[activeArray.length - 1].length;
-    }
-    numCounter = 0;
-}
-
-function removeNumber() {
-    popActiveArrayNum();
-    if(checkPlusMinusState() && numCounter <= 1) {
-        togglePlusMinus();
-    }
-    activeString = activeString.slice(0, activeString.length - 1)
-    --numCounter
-    if(numCounter === 0 && activeArray.length > 0) {
-        lastOperatorLength = activeArray[activeArray.length - 1].length;
-    }
-}
-
-// EQUAL FUNCTIONS
-function triggerEqual() {
-    if(activeString !== "" && lastOperatorLength === 0 && !checkOpenParenthesis()){
-        pushString();
-        while (parenthesisCount > 0) {
-            activeArray.push(closedParenthesis);
-            activeString += closedParenthesis;
-            --parenthesisCount;       
-        }
-        executeCalculate();
-    }
-    else if(activeString === "" && Object.keys(history).length > 0) {
-        activeArray = history[Object.keys(history).length][2];
-        activeString = history[Object.keys(history).length] [0];
-        executeCalculate();
-    }
-}
-
-function executeCalculate() {
-    let tempArray = activeArray.slice(0);
-    ans.value = calculate(tempArray)
-    calculationText.innerText = `${activeString} =`;
-    if (isNaN(ans.value)) {
-        activeText.innerText = errorMessage;
-        ans.value = undefined;
-    }
-    else {
-        activeText.innerText = Math.round(100000000 * ans.value)/100000000;
-    }
-    addToHistory()
-    resetVariables()
-}
-
-function addToHistory() {
-    history[`${Object.keys(history).length+1}`] = [activeString, ans.value, activeArray]
-    let newPara = document.createElement("p");
-    let paraText = document.createTextNode(`${activeString} = ${activeText.innerText}`)
-    newPara.appendChild(paraText)
-    historyText.appendChild(newPara)
-}
-
-function resetVariables() {
-    activeArray = [];
-    activeString = "";
-    numCounter = 0;
-    lastOperatorLength = 0;
-    parenthesisCount = 0;
-}
-
-function clearAll() {
-    resetVariables()
-    ans.value = undefined;
-    activeText.innerText = "";
-    calculationText.innerText = "";
-}
-
-/* HISTORY BUTTONS AND EVENT HANDLERS */
+    /* 
+    HISTORY BUTTONS AND HANDLERS 
+    */
 function clearHistory() {
     history = {}
     while (historyText.firstChild) {
